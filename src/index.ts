5a288c9a9ad7453fdb6f4d64d9908095209493e2
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-10-09 11:59:36 +0200

    made-a-fizzbuzz-function-to-test-the-tests
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hej då');
+export const fizzBuzz = (fizzArray: unknown[]) => {
+    if (fizzArray.length === 0) console.log("please provide a number. cant be an empty array");
+
+    if (fizzArray[0] === 1) console.log("1");
+
+};
