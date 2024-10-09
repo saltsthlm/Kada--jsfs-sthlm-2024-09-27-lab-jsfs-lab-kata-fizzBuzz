a4568c08ecbb1ad8c1c215b20073ee1931f2af2b
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-10-09 12:22:28 +0200

    made-tests-for-the-fizzbuz-function
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,7 @@ import test from "node:test";
 import { fizzBuzz } from ".";
 
 
-test.skip("Write a trivial test that will fail", () => {
+test("Write a trivial test that will fail", () => {
 ok(true);
 });
 
@@ -17,44 +17,44 @@ test("should log empty array",() => {
     deepEqual(output, "please provide a number. cant be an empty array");
 });
 
-
-test.skip("should log empty array",() => {
+test.skip("Should log number 1 from array",() => {
     let output = "";
     console.log = (msg) => { 
         output = msg;
     };
 
     fizzBuzz([]);
-    deepEqual(output, "please provide a number. cant be an empty array");
-});
-
-test("Should log number 1 from array",() => {
-    let output = "";
-    console.log = (msg) => { 
-        output = msg;
-    };
-
-    fizzBuzz([1]);
     deepEqual(output, "1");
 });
 
-test.skip("should log empty array",() => {
+test("Should provide a number.",() => {
     let output = "";
     console.log = (msg) => { 
         output = msg;
     };
 
-    fizzBuzz([]);
-    deepEqual(output, "please provide a number. cant be an empty array");
+    fizzBuzz(["banan"]);
+    deepEqual(output, "Please provide a number. Try again");
+});
+
+test("Should provide a positive number.",() => {
+    let output = "";
+    console.log = (msg) => { 
+        output = msg;
+    };
+
+    fizzBuzz([-2]);
+    deepEqual(output, "Please provide a positive number. Try again");
 });
 
 
-test.skip("Should log number 1",() => {
-    const result = fizzBuzz([0]);
-    deepEqual(result, 1);
+test("Should provide a number below 100.",() => {
+    let output = "";
+    console.log = (msg) => { 
+        output = msg;
+    };
+
+    fizzBuzz([2215]);
+    deepEqual(output, "Please provide a number below 100");
 });
 
-test.skip("Should return log for number 3", () => {
-    const result = fizzBuzz([]);
-    deepEqual(result, 3);
-});
