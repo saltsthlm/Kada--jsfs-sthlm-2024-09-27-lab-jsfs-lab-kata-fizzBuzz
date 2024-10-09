448803647581ff39504cc42343ffe06552478409
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-10-09 12:35:31 +0200

    made-function-be-able-to-handle-differnet-scinarios
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ export const fizzBuzz = (fizzArray: (number | string)[] ) => {
         
         if (typeof element === "number" && element < 0) console.log("Please provide a positive number. Try again");
 
-        if (typeof element === "number" && element > 100)console.log("Please provide a number below 100");
+        if (typeof element === "number" && element > 100) console.log("Please provide a number below 100");
+
+        if (typeof element === "number" && element % 15 === 0) console.log("FizzBuzz");
+
+        if (typeof element === "number" && element % 3 === 0 && element % 15 !== 0) console.log("Fizz");
+
+        if (typeof element === "number" && element % 5 === 0 && element % 15 !== 0) console.log("Buzz");
     });
 };
+
+fizzBuzz([3000000]);
