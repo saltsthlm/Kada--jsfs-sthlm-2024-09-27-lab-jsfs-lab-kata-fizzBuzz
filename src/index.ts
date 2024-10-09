81f9e4be72e7fea151c8b7babc661be3c9ff8fad
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-10-09 12:15:12 +0200

    made-a-fizzbuzz-function
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-export const fizzBuzz = (fizzArray: unknown[]) => {
+export const fizzBuzz = (fizzArray: (number | string)[] ) => {
     if (fizzArray.length === 0) console.log("please provide a number. cant be an empty array");
+    fizzArray.forEach(element => {
+        if (typeof element !== "number") console.log("Please provide a number. Try again");
+        
+        if (typeof element === "number" && element < 0) console.log("Please provide a positive number. Try again");
 
-    if (fizzArray[0] === 1) console.log("1");
-
+        if (typeof element === "number" && element > 100)console.log("Please provide a number below 100");
+    });
 };
