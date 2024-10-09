796426136c7ebea8e0e5da35b7914dc11a0999dc
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-10-09 11:59:15 +0200

    made-3-tests
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,60 @@
-import { ok } from "node:assert/strict";
+import { deepEqual, ok } from "node:assert/strict";
 import test from "node:test";
+import { fizzBuzz } from ".";
 
-test("Write a trivial test that will fail", () => {
+
+test.skip("Write a trivial test that will fail", () => {
 ok(true);
+});
+
+test("should log empty array",() => {
+    let output = "";
+    console.log = (msg) => { 
+        output = msg;
+    };
+
+    fizzBuzz([]);
+    deepEqual(output, "please provide a number. cant be an empty array");
+});
+
+
+test.skip("should log empty array",() => {
+    let output = "";
+    console.log = (msg) => { 
+        output = msg;
+    };
+
+    fizzBuzz([]);
+    deepEqual(output, "please provide a number. cant be an empty array");
+});
+
+test("Should log number 1 from array",() => {
+    let output = "";
+    console.log = (msg) => { 
+        output = msg;
+    };
+
+    fizzBuzz([1]);
+    deepEqual(output, "1");
+});
+
+test.skip("should log empty array",() => {
+    let output = "";
+    console.log = (msg) => { 
+        output = msg;
+    };
+
+    fizzBuzz([]);
+    deepEqual(output, "please provide a number. cant be an empty array");
+});
+
+
+test.skip("Should log number 1",() => {
+    const result = fizzBuzz([0]);
+    deepEqual(result, 1);
+});
+
+test.skip("Should return log for number 3", () => {
+    const result = fizzBuzz([]);
+    deepEqual(result, 3);
 });
